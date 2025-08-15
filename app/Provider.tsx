2c0a59f43762866712929fdd08d14e591e838308
ADARSH CHOUKSEY <choukseyaryan49@gmail.com>
2025-08-15 18:28:20 +0530

    database setup completed

---
+++ app/Provider.tsx
@@ -0,0 +1,44 @@
+"use client"
+import { userDetailContext } from '@/context/UserDetailContext';
+import { api } from '@/convex/_generated/api';
+import { useUser } from '@clerk/nextjs';
+import { useMutation } from 'convex/react'
+import { createContext } from 'react';
+import React, { useEffect, useState } from 'react'
+
+
+function Provider({children}:any) {
+   const {user}=useUser();
+    const CreateUser= useMutation(api.users.CreateNewUser);
+    const [userDetail,setUserDetail]=useState<any>();
+    console.log('user')
+ 
+    useEffect(()=>{
+user&& CreateNewUser();
+    },[user])
+ 
+    const CreateNewUser=async()=>{
+
+        if (user){
+        const result=await CreateUser({
+            email:user?.primaryEmailAddress?.emailAddress??'',
+            imageUrl:user?.imageUrl,
+            name:user?.fullName??'',
+        });
+        console.log(result)
+        setUserDetail(result);
+    }
+    }
+
+  return (
+    <userDetailContext.Provider value={{userDetail,setUserDetail}}>
+    <div>{children}</div>
+    </userDetailContext.Provider>
+  )
+}
+
+export default Provider
+
+export const useUserDetailContext=()=>{
+    return createContext(userDetailContext);
+}
